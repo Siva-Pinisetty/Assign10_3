f1f812807917fd106bbce374254e3bde1e5a4e1f
balu-pinisetty <balupinisetty@gmail.com>
2021-05-27 00:21:07 +0530

    Winning Percentage of Singlet Combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,10 +1,23 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "Flip Coin Combination Problem"
+declare -A dict
+read -p "Enter the number to flip coin: " nmb
 
-ran=$((RANDOM%2))
-if [ $ran -eq 1 ]
-then
-	echo "Coin faces HEAD"
-else
-	echo "Coin faces TAIL"
-fi
+for ((i=1; i<=$nmb; i++))
+do
+	ran=$((RANDOM%2))
+	if [ $ran -eq 1 ]
+	then
+		((++dict[H]))
+	else
+		((++dict[T]))
+	fi
+done
+echo "By flipping the coin $nmb times"
+echo "it faces Head ${dict[H]} times & Tail ${dict[T]} times"
+
+win_head=$( perl -e "{print (${dict[H]}/$nmb * 100)}")
+win_tail=$( perl -e "{print (${dict[T]}/$nmb * 100)}")
+
+echo "Percentage of Winning Head is $win_head"
+echo "Percentage of Winning Tail is $win_tail"
